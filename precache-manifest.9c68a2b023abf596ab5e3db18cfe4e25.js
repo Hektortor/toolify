867e0594ded9e4ff58a959c7567a64ca89ce4092
Hektortor <viktor.frohnapfel1@gmail.com>
2019-05-03 20:49:51 +0200

    Updates

---
+++ precache-manifest.9c68a2b023abf596ab5e3db18cfe4e25.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88fd64c0fcf14475eee8a4dd2b94e9ee",
+    "url": "/spotify-tools/index.html"
+  },
+  {
+    "revision": "231f544f833b04680889",
+    "url": "/spotify-tools/static/css/main.8642abdd.chunk.css"
+  },
+  {
+    "revision": "a41a40e11093d811d5c8",
+    "url": "/spotify-tools/static/js/2.6b3708a6.chunk.js"
+  },
+  {
+    "revision": "231f544f833b04680889",
+    "url": "/spotify-tools/static/js/main.ca62a5f4.chunk.js"
+  },
+  {
+    "revision": "1a669d50205f29c6737d",
+    "url": "/spotify-tools/static/js/runtime~main.cc055dca.js"
+  },
+  {
+    "revision": "e29998d86afd3d3d0dfc1198290ef8c3",
+    "url": "/spotify-tools/static/media/Gotham-Black.e29998d8.woff"
+  },
+  {
+    "revision": "b51ec430e735ebae2eddaa4f03675d56",
+    "url": "/spotify-tools/static/media/Gotham-Bold.b51ec430.woff"
+  },
+  {
+    "revision": "cec78cc5a1d901e4874e3d9feebb7646",
+    "url": "/spotify-tools/static/media/Gotham-BookItalic.cec78cc5.woff"
+  },
+  {
+    "revision": "dcb8a07cf3cbc95e4fc326dda7bf4329",
+    "url": "/spotify-tools/static/media/Gotham-Light.dcb8a07c.woff"
+  },
+  {
+    "revision": "cae9f840575e702faf5229839f268f9e",
+    "url": "/spotify-tools/static/media/Gotham-Thin.cae9f840.woff"
+  },
+  {
+    "revision": "9633c2743683e1725e07925834204905",
+    "url": "/spotify-tools/static/media/Gotham-ThinItalic.9633c274.woff"
+  },
+  {
+    "revision": "a5048d9425015d68823a9879e49a1681",
+    "url": "/spotify-tools/static/media/Gotham-UltraItalic.a5048d94.woff"
+  },
+  {
+    "revision": "c614fbadff73fc2f0825661e872a94be",
+    "url": "/spotify-tools/static/media/Gotham-XLight.c614fbad.woff"
+  },
+  {
+    "revision": "a2a0f26729aefc3a01b6fde552fe6c5e",
+    "url": "/spotify-tools/static/media/Gotham-XLightItalic.a2a0f267.woff"
+  },
+  {
+    "revision": "39b1c119caf33ce82b61e6b5f6a6c53e",
+    "url": "/spotify-tools/static/media/GothamBold.39b1c119.woff"
+  },
+  {
+    "revision": "4091bfea289f4d7a9feb495860909170",
+    "url": "/spotify-tools/static/media/GothamBoldItalic.4091bfea.woff"
+  },
+  {
+    "revision": "41eebc608a217814063e8b16a285beb7",
+    "url": "/spotify-tools/static/media/GothamBook.41eebc60.woff"
+  },
+  {
+    "revision": "50aed062699526d3b2fc25f9bdac3529",
+    "url": "/spotify-tools/static/media/GothamBookItalic.50aed062.woff"
+  },
+  {
+    "revision": "f0de7009e4f5750463854369de46427b",
+    "url": "/spotify-tools/static/media/GothamLight.f0de7009.woff"
+  },
+  {
+    "revision": "7f5118ac8a719c29c554c5d9638b1cec",
+    "url": "/spotify-tools/static/media/GothamLightItalic.7f5118ac.woff"
+  },
+  {
+    "revision": "ba7ee4ffcdcd65f753a35cd804ac1514",
+    "url": "/spotify-tools/static/media/GothamMedium.ba7ee4ff.woff"
+  },
+  {
+    "revision": "3718793abbcecd542fb00b77f5027d33",
+    "url": "/spotify-tools/static/media/GothamMediumItalic.3718793a.woff"
+  },
+  {
+    "revision": "ba7ee4ffcdcd65f753a35cd804ac1514",
+    "url": "/spotify-tools/static/media/GothamMedium_1.ba7ee4ff.woff"
+  }
+]);
