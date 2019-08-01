9870e3ac0fa88875fb69981ac55d9fd740b7ab02
Hektortor <viktor.frohnapfel1@gmail.com>
2019-08-01 12:53:50 +0200

    update dependencies

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import packageJson from '../package.json';
 import '../App.css';
 import author from '../img/author.jpg';
 import { withRouter } from 'react-router-dom';
@@ -9,7 +8,7 @@ class About extends Component {
   render() {
     return (
       <div className="container" style={{ fontFamily: 'Gotham Bold', textAlign: 'left' }}>
-        <h1 className="title">Toolify { packageJson.version }</h1>
+        <h1 className="title">Toolify 1.1.8</h1>
         <h5 style={{ fontFamily: 'Gotham Medium' }}>Toolify is a collection of extra functionality for Spotify!</h5>
         <h3 style={{ fontFamily: 'Gotham Medium' }} className="title">Made with ♥ by</h3>
         <div>
